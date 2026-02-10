37915385a7ed2159fab3451e737fde14731d11d9
ShivkanyaPimple96 <altwiseshivani@gmail.com>
2026-02-10 18:08:03 +0530

    last updated only added commented date

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:pcmc_jeevan_praman/splash_screen.dart';
 
+// letest update 10-02-2026
 void main() {
   // Override global HTTP settings to bypass SSL certificate verification
   HttpOverrides.global = MyHttpOverrides();
